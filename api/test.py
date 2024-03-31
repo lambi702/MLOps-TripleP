1eaca376199bd8bea1fa629a6c94e450e08a2ffb
Romain Lambermont <romain.lambermont@student.uliege.be>
2024-03-31 17:05:37 +0200

    api

--- api/test.py
+++ api/test.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
-def get_from_model(date: str) -> str:
-    return date
+@app.route('/')
+def upload_file():
+    to_return = '''
+    <html>
+        <body>
+            <form action = "/uploader" method = "POST" 
+                enctype = "multipart/form-data">
+                <input type = "file" name = "file" />
+                <input type = "submit"/>
+            </form>         
+        </body>
+    </html>
+    '''
+    return to_return
 
-@app.route('/<date>')
-def hello(date: str):
-    return f'Hello, {get_from_model(date)}!'
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file_post():
+    if request.method == 'POST':
+        f = request.files['file']
+        data = json.load(f)
+        return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug = True, host='0.0.0.0')
